05a064bdaa9deadd4dd84c967c9bc8829465ef9c
TATA <hostit313@gmail.com>
2023-10-11 23:38:05 +0700

    update headers

--- cors-proxy.js
+++ cors-proxy.js
@@ -1,8 +1,25 @@
 const axios = require("axios");
 
 exports.handler = async function (event, context) {
+  // Handle the preflight request
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      },
+      body: "CORS preflight successful",
+    };
+  }
+
   if (event.httpMethod !== "GET") {
-    return { statusCode: 405, body: "Method Not Allowed" };
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+      headers: { "Access-Control-Allow-Origin": "*" },
+    };
   }
 
   const targetURL = event.queryStringParameters.url;
@@ -11,9 +28,7 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 400,
       body: JSON.stringify({ error: "url query param is required" }),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
+      headers: { "Access-Control-Allow-Origin": "*" },
     };
   }
 
@@ -31,9 +46,7 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Failed to fetch from target URL" }),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
+      headers: { "Access-Control-Allow-Origin": "*" },
     };
   }
 };
